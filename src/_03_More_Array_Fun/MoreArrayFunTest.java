9d11223e176b1e267bebb32ff0d7c63d70a11082
EmperorlNeo <league@Admins-Mac-mini-2.local>
2018-12-29 11:21:50 -0800

    Fixed errors, separated tests
    tests are now formatted as: “reversePrinterWorks_fourStrings” for a
    single case as opposed to “testEvenPrinter” covering all test cases.

--- src/_03_More_Array_Fun/MoreArrayFunTest.java
+++ src/_03_More_Array_Fun/MoreArrayFunTest.java
@@ -6,33 +6,105 @@ import org.junit.jupiter.api.Test;
 
 class MoreArrayFunTest {
 
+	// ARRAY STRING PRINTER
+
 	@Test
-	public void testArrayPrinter() {
-		String[] array = {"blue", "cello", "cactus", "electric"};
+	public void arrayPrinterWorks_fourStrings() {
+		String[] array = { "blue", "cello", "cactus", "electric" };
 		String allValues = MoreArrayFun.stringArrayPrinter(array);
 		assertEquals("blue, cello, cactus, electric", allValues);
-		
-		String[] arrayTwo = {"snow leopard enclosure", "bean paste candy"};
+
+	}
+
+	@Test
+	public void arrayPrinterWorks_twoStringsWithSpaces() {
+
+		String[] arrayTwo = { "snow leopard enclosure", "bean paste candy" };
 		String allValuesTwo = MoreArrayFun.stringArrayPrinter(arrayTwo);
 		assertEquals("snow leopard enclosure, bean paste candy", allValuesTwo);
-		
-		String[] arrayThree = {"straw", "velvet", "chocolate", "chrome", "buttons", "orange juice", "mediterranian", "sample", "green", "silt", "carpet", "paw print", "soft", "ingenuity", "clamshell", "pink", "difficult"};
-		String allValuesThree = MoreArrayFun.stringArrayPrinter(arrayThree);
-		assertEquals("straw, velvet, chocolate, chrome, buttons, orange juice, mediterranian, sample, green, silt, carpet, paw print, soft, ingenuity, clamshell, pink, difficult", allValuesThree);
+
 	}
-	
+
 	@Test
-	public void testEvenIndexPrinter() {
-		String[] array = {"treetop", "snout", "stoplight", "enigma"};
+	public void arrayPrinterWorks_lotsOfStrings() {
+		String[] arrayThree = { "straw", "velvet", "chocolate", "chrome", "buttons", "orange juice", "mediterranian",
+				"sample", "green", "silt", "carpet", "paw print", "soft", "ingenuity", "clamshell", "pink",
+				"difficult" };
+		String allValuesThree = MoreArrayFun.stringArrayPrinter(arrayThree);
+		assertEquals(
+				"straw, velvet, chocolate, chrome, buttons, orange juice, mediterranian, sample, green, silt, carpet, paw print, soft, ingenuity, clamshell, pink, difficult",
+				allValuesThree);
+	}
+
+	@Test
+	public void arrayPrinterWorks_blankString() {
+		String[] arrayFour = { "" };
+		String allValuesFour = MoreArrayFun.stringArrayPrinter(arrayFour);
+		assertEquals("", allValuesFour);
+	}
+
+	// EVEN STRING PRINTER
+
+	@Test
+	public void evenPrinterWorks_fourStrings() {
+		String[] array = { "treetop", "snout", "stoplight", "enigma" };
 		String allValues = MoreArrayFun.stringArrayEvenIndexPrinter(array);
 		assertEquals("snout, enigma", allValues);
+
 	}
-	
+
 	@Test
-	public void testReverseIndexPrinter() {
-		String[] array = {"star", "cheese", "twelve", "ashes"};
+	public void evenPrinterWorks_sevenStrings() {
+		String[] array2 = { "strong", "fermenting", "glass", "crunchy", "smooth", "sweet", "rare" };
+		String allValuesForArray2 = MoreArrayFun.stringArrayEvenIndexPrinter(array2);
+		assertEquals("fermenting, crunchy, sweet", allValuesForArray2);
+	}
+
+	@Test
+	public void evenPrinterWorks_oneString() {
+		String[] array3 = { "worm" };
+		String allValuesFor3 = MoreArrayFun.stringArrayEvenIndexPrinter(array3);
+		assertEquals("", allValuesFor3);
+	}
+
+	@Test
+	public void evenPrinterWorks_blankString() {
+		String[] arrayFour = { "" };
+		String allValuesFour = MoreArrayFun.stringArrayEvenIndexPrinter(arrayFour);
+		assertEquals("", allValuesFour);
+	}
+
+	// REVERSE STRING PRINTER
+
+	@Test
+	public void reversePrinterWorks_fourStrings() {
+		String[] array = { "star", "cheese", "twelve", "ashes" };
 		String allValues = MoreArrayFun.stringArrayReverseOrderPrinter(array);
 		assertEquals("ashes, twelve, cheese, star", allValues);
+
+	}
+
+	@Test
+	public void reversePrinterWorks_oneString() {
+		String[] array2 = { "purple" };
+		String allValuesForArrayTwo = MoreArrayFun.stringArrayReverseOrderPrinter(array2);
+		assertEquals("purple", allValuesForArrayTwo);
+
+	}
+
+	@Test
+	public void reversePrinterWorks_twoStrings() {
+		String[] array3 = { "trash", "recycle", "compost" };
+		String allValuesForArrayThree = MoreArrayFun.stringArrayReverseOrderPrinter(array3);
+		assertEquals("compost, recycle, trash", allValuesForArrayThree);
+
+	}
+
+	@Test
+	public void reversePrinterWorks_blankString() {
+		String[] arrayFour = { "" };
+		String allValuesFour = MoreArrayFun.stringArrayReverseOrderPrinter(arrayFour);
+		assertEquals("", allValuesFour);
 	}
 
 }
